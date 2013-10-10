59b8448cc347610ff4e46caf1ad330420e9db678
Daemon <cgyqqcgy@126.com>
2013-10-10 17:19:12 +0800

    test mongodb in go

--- src/web/server.go
+++ src/web/server.go
@@ -2,22 +2,54 @@ package main
 
 import (
 	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"log"
 	"net/http"
 	// "strings"
 )
 
-func logRequeset(req http.ResponseWriter, res *http.Request) {
-	res.ParseForm()
-	fmt.Println(res.Form)
-	fmt.Println(res.URL.Path)
-	fmt.Fprintf(req, "test")
+func logRequest(res http.ResponseWriter, req *http.Request) {
+	fmt.Print("aa")
+	req.ParseForm()
+	fmt.Println(req.Form)
+	fmt.Println(req.URL.Path)
+	fmt.Fprintf(res, "test")
+}
+
+type Person struct {
+	Name  string
+	Phone string
 }
 
 func main() {
-	http.HandleFunc("/", logRequeset)
+	http.HandleFunc("/", logRequest)
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
 		log.Fatal("error on server", err)
 	}
+	fmt.Print("start server on port 9999")
+
+	session, err := mgo.Dial("http://192.168.27.108:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("customer_behavior").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		panic(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
 }
