5ff828047e984e8b60a26e9f8664623d6248cba1
Daemon <cgyqqcgy@126.com>
2013-10-10 17:54:42 +0800

    fixed bug that connect to mongodb

--- src/web/server.go
+++ src/web/server.go
@@ -9,12 +9,43 @@ import (
 	// "strings"
 )
 
+func dbTest() {
+	session, err := mgo.Dial("localhost")
+	fmt.Print("dbTest start")
+	if err != nil {
+		fmt.Print("connect mongo error:")
+		fmt.Print(err)
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("customer_behavior").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		fmt.Print(err)
+
+		panic(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		fmt.Print(err)
+
+		panic(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+}
 func logRequest(res http.ResponseWriter, req *http.Request) {
-	fmt.Print("aa")
 	req.ParseForm()
 	fmt.Println(req.Form)
 	fmt.Println(req.URL.Path)
 	fmt.Fprintf(res, "test")
+	dbTest()
 }
 
 type Person struct {
@@ -30,26 +61,4 @@ func main() {
 	}
 	fmt.Print("start server on port 9999")
 
-	session, err := mgo.Dial("http://192.168.27.108:27017")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("customer_behavior").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		panic(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
 }
