233f5d08f751041c04c088b910d375981c24d9a5
Daemon for Mac <cgyqqcgy@126.com>
2013-12-03 14:14:25 +0800

    add  new UI

--- src/sayhellobygoroutine/hello.go
+++ src/sayhellobygoroutine/hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	// "os/exec"
 	"runtime"
 )
 
@@ -13,11 +13,23 @@ func hello(s string) {
 		fmt.Println(runtime.NumCPU())
 	}
 }
-
+func iteratorTest() {
+	m := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	//slice
+	s := make([]int, len(m))
+	i := 0
+	for k, _ := range m {
+		s[i] = k
+		i++
+	}
+	fmt.Println("iterator test", s, len(m))
+	// fmt.Println(s)
+}
 func main() {
 	go hello("1")
 	hello("2")
-	cmd := exec.Command("shutdown", "-f")
-	cmd.Run()
-	// hello("3")
+	// cmd := exec.Command("shutdown", "-f")
+	// cmd.Run()
+	hello("3")
+	iteratorTest()
 }
