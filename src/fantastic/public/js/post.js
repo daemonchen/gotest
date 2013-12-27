6b6cd06f34c3e20ce15a7fa6d2f73e722b588b3b
Daemon for Mac <cgyqqcgy@126.com>
2013-12-27 09:46:53 +0800

    add yesterday  js

---
+++ src/fantastic/public/js/post.js
@@ -0,0 +1,38 @@
+semantic.sidebar = {};
+semantic.sidebar.ready = function(){
+  $('.sidebar').first().sidebar('attach events', '#menu');
+};
+$(document)
+  .ready(semantic.sidebar.ready)
+;
+
+var formatTimeLabel = function(){
+  var stamp = parseInt($("#stamp").html())
+  // moment(stamp*1000).format("YYYY:MM:DD  hh:mm:ss")
+  $("#time").html(moment(stamp*1000).format("YYYY年MM月DD日  hh:mm:ss"))
+}
+$(function(){
+  formatTimeLabel()
+  $("#save").click(function(){
+    var contentEditable = CKEDITOR.instances.editor
+    if(contentEditable == undefined){return}
+    var stamp = $("#stamp").html()
+    var articleContent = contentEditable.getData()
+    if(articleContent.length == 0){
+      console.log("你在逗我么");
+    }
+    $.ajax({
+        url: "/edit/update",
+        type: "POST",
+        data: { stamp: stamp, content: articleContent},
+        dataType: "json",
+        success: function (result) {
+          console.log("todo: change url to detail","result:",result);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status);
+        console.log(thrownError);
+        }
+    });
+  })
+});
