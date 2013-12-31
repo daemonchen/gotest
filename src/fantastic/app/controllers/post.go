a7db8211eb9c75bc40ed3de10b20427362833934
daemon <cgyqqcgy@126.com>
2013-12-31 22:25:11 +0800

    add hashkey when load post page

--- src/fantastic/app/controllers/post.go
+++ src/fantastic/app/controllers/post.go
@@ -27,9 +27,9 @@ type Post struct {
 // 	CommentText   string `json:"commentText"`
 // 	CommentTime   string `json:"commentTime"`
 // }
-func (c *Post) generateSessionKey(data string) []byte {
+func (c *Post) generateSessionKey() []byte {
 	md5Key := md5.New()
-	io.WriteString(md5Key, data)
+	io.WriteString(md5Key, "this is my first hash session key")
 	return md5Key.Sum([]byte("daemonchen"))
 
 }
@@ -38,8 +38,8 @@ func (c *Post) Index(stamp string) revel.Result {
 	isLogin := c.Session["islogin"]
 
 	randNum := rand.Int63n(time.Now().Unix())
-	hashKey := c.generateSessionKey("this is my first hash session key")
-	revel.WARN.Println("randNum:", randNum, hashKey)
+	hashKey := c.generateSessionKey()
+	c.Session[string(hashKey[:])] = randNum
 
 	post := models.GetPostByStamp(c.MongoSession, stamp)
 	comments := models.GetCommentsByStamp(c.MongoSession, stamp)
@@ -58,14 +58,24 @@ func (c *Post) Update(stamp string, content string) revel.Result {
 	return c.RenderJson(responseJson)
 }
 
+func (c *Post) clearCommentSession() {
+	hashKey := c.generateSessionKey()
+	c.Session[string(hashKey[:])] = nil
+}
 func (c *Post) AddComment(commentData string) revel.Result {
-	responseJson := &BayesLearnResult{"success comment", "success comment"}
-	// var comment Comment
-	// err := json.Unmarshal([]byte(commentData), &comment)
+	hashKey := c.generateSessionKey()
+	if c.Session[string(hashKey[:])] == nil{
+		c.Response.Status = 403
+		return c.RenderJson(&BayesLearnResult{"failed", "you can't comment now, please wait for a moment")
+	}
+
 	err := models.SaveComment(c.MongoSession, commentData)
 	if err != nil {
 		revel.WARN.Println("occur err when update:", err)
-		return c.RenderJson(&BayesLearnResult{"insert comment failed", "failed comment"})
+		c.Response.Status = 403
+		return c.RenderJson(&BayesLearnResult{"failed", "insert comment failed"})
 	}
-	return c.RenderJson(responseJson)
+	c.clearCommentSession()
+	c.Response.Status = 200
+	return c.RenderJson(&BayesLearnResult{"success", "insert comment success"})
 }
