e3929a565ce01871e9d26e760bf16701baaf52ce
daemon <cgyqqcgy@126.com>
2013-12-31 23:20:39 +0800

    fixed bug stuff

--- src/fantastic/app/controllers/post.go
+++ src/fantastic/app/controllers/post.go
@@ -20,6 +20,8 @@ type Post struct {
 	revmgo.MongoController
 }
 
+type CommentCache bool
+
 func (c *Post) generateSessionKey() []byte {
 	md5Key := md5.New()
 	io.WriteString(md5Key, "this is my first hash session key")
@@ -33,7 +35,7 @@ func (c *Post) Index(stamp string) revel.Result {
 	randNum := rand.Int63n(time.Now().Unix())
 	hashKey := c.generateSessionKey()
 	c.Session[string(hashKey[:])] = strconv.FormatInt(randNum, 10)
-	cache.Set(strconv.FormatInt(randNum, 10), "true", 30*time.Minute)
+	cache.Set(strconv.FormatInt(randNum, 10), true, 30*time.Minute)
 
 	post := models.GetPostByStamp(c.MongoSession, stamp)
 	comments := models.GetCommentsByStamp(c.MongoSession, stamp)
@@ -57,19 +59,23 @@ func (c *Post) clearCommentCacheValue() {
 }
 func (c *Post) AddComment(commentData string) revel.Result {
 	hashKey := c.generateSessionKey()
-	if cacheErr := cache.Get(c.Session[string(hashKey[:])]); cacheErr != nil {
+	var commentCache CommentCache
+	if cacheErr := cache.Get(c.Session[string(hashKey[:])], &commentCache); cacheErr != nil {
+		err := models.SaveComment(c.MongoSession, commentData)
+		if err != nil {
+			revel.WARN.Println("occur err when update:", err)
+			c.Response.Status = 403
+			return c.RenderJson(&BayesLearnResult{"failed", "insert comment failed"})
+		}
+		c.clearCommentCacheValue()
+		c.Response.Status = 200
+		return c.RenderJson(&BayesLearnResult{"success", "insert comment success"})
+
+	} else {
 		revel.WARN.Println("cache get err:", cacheErr)
 		c.Response.Status = 403
 		return c.RenderJson(&BayesLearnResult{"failed", "you can't comment now, please refresh page and wait for a moment"})
+
 	}
 
-	err := models.SaveComment(c.MongoSession, commentData)
-	if err != nil {
-		revel.WARN.Println("occur err when update:", err)
-		c.Response.Status = 403
-		return c.RenderJson(&BayesLearnResult{"failed", "insert comment failed"})
-	}
-	c.clearCommentCacheValue()
-	c.Response.Status = 200
-	return c.RenderJson(&BayesLearnResult{"success", "insert comment success"})
 }
